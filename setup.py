07f7fa5b8f010b4de9b676f5aa0e670dab5c3fd6
jordiup <jordi.hermoso.up@gmail.com>
2023-01-12 16:05:40 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "jhc_cf_sdk_test"
-VERSION = "1.2.4"
+VERSION = "1.2.5"
 # To install the library, run the following
 #
 # python setup.py install
